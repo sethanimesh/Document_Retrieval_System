045d95813ba87dedfae2e5178d7d9e6456f99037
animesh <sethanimesh@hotmail.com>
2024-09-15 02:36:37 +0530

    SQL Table

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users_details'
+
+    user_id = Column(String, primary_key=True)
+    request_count = Column(Integer, default=1)
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
