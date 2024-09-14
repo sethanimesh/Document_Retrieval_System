b9a6eae0f0075528de97de34dac4339c6be4fe6e
animesh <sethanimesh@hotmail.com>
2024-09-15 02:53:33 +0530

    fixing user_id datatype

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users_details'
 
-    user_id = Column(String, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     request_count = Column(Integer, default=1)
 
 def create_tables(engine):
